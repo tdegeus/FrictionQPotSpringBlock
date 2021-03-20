99a29a15623b31fa40c25dc67933bd4103318884
Tom de Geus <tom@geus.me>
2021-03-20 16:50:00 +0100

    inlining functions

--- include/FrictionQPotSpringBlock/Line1d.hpp
+++ include/FrictionQPotSpringBlock/Line1d.hpp
@@ -31,7 +31,7 @@ inline std::vector<std::string> version_dependencies()
 }
 
 template <class F>
-System::System(size_t N, F func) : m_N(N)
+inline System::System(size_t N, F func) : m_N(N)
 {
     m_f = xt::zeros<double>({m_N});
     m_f_potential = xt::zeros<double>({m_N});
@@ -55,49 +55,49 @@ System::System(size_t N, F func) : m_N(N)
     this->computeForce();
 }
 
-size_t System::N() const
+inline size_t System::N() const
 {
     return m_N;
 }
 
-void System::set_dt(double arg)
+inline void System::set_dt(double arg)
 {
     m_dt = arg;
 }
 
-void System::set_eta(double arg)
+inline void System::set_eta(double arg)
 {
     m_eta = arg;
 }
 
-void System::set_m(double arg)
+inline void System::set_m(double arg)
 {
     m_m = arg;
 }
 
-void System::set_mu(double arg)
+inline void System::set_mu(double arg)
 {
     m_mu = arg;
 }
 
-void System::set_k_neighbours(double arg)
+inline void System::set_k_neighbours(double arg)
 {
     m_k_neighbours = arg;
 }
 
-void System::set_k_frame(double arg)
+inline void System::set_k_frame(double arg)
 {
     m_k_frame = arg;
 }
 
-void System::set_x_frame(double arg)
+inline void System::set_x_frame(double arg)
 {
     m_x_frame = arg;
     this->computeForceFrame();
     this->computeForce();
 }
 
-bool System::set_x(const xt::xtensor<double, 1>& arg)
+inline bool System::set_x(const xt::xtensor<double, 1>& arg)
 {
     FRICTIONQPOTSPRINGBLOCK_ASSERT(arg.size() == m_N);
     xt::noalias(m_x) = arg;
@@ -108,7 +108,7 @@ bool System::set_x(const xt::xtensor<double, 1>& arg)
     return redraw;
 }
 
-void System::set_v(const xt::xtensor<double, 1>& arg)
+inline void System::set_v(const xt::xtensor<double, 1>& arg)
 {
     FRICTIONQPOTSPRINGBLOCK_ASSERT(arg.size() == m_N);
     xt::noalias(m_v) = arg;
@@ -116,13 +116,13 @@ void System::set_v(const xt::xtensor<double, 1>& arg)
     this->computeForce();
 }
 
-void System::set_a(const xt::xtensor<double, 1>& arg)
+inline void System::set_a(const xt::xtensor<double, 1>& arg)
 {
     FRICTIONQPOTSPRINGBLOCK_ASSERT(arg.size() == m_N);
     xt::noalias(m_a) = arg;
 }
 
-bool System::computeForcePotential()
+inline bool System::computeForcePotential()
 {
     bool redraw = m_y.setPosition(m_x);
     xt::noalias(m_y_l) = m_y.currentYieldLeft();
@@ -131,7 +131,7 @@ bool System::computeForcePotential()
     return redraw;
 }
 
-void System::computeForceNeighbours()
+inline void System::computeForceNeighbours()
 {
     for (size_t p = 1; p < m_N - 1; ++p) {
         m_f_neighbours(p) = m_k_neighbours * (m_x(p - 1) - 2 * m_x(p) + m_x(p + 1));
@@ -140,22 +140,22 @@ void System::computeForceNeighbours()
     m_f_neighbours(m_N - 1) = m_k_neighbours * (m_x(m_N - 2) - 2 * m_x(m_N - 1) + m_x(0));
 }
 
-void System::computeForceFrame()
+inline void System::computeForceFrame()
 {
     xt::noalias(m_f_frame) = m_k_frame * (m_x_frame - m_x);
 }
 
-void System::computeForceDamping()
+inline void System::computeForceDamping()
 {
     xt::noalias(m_f_damping) = - m_eta * m_v;
 }
 
-void System::computeForce()
+inline void System::computeForce()
 {
     xt::noalias(m_f) = m_f_potential + m_f_neighbours + m_f_damping + m_f_frame;
 }
 
-bool System::timeStep()
+inline bool System::timeStep()
 {
     xt::noalias(m_v_n) = m_v;
     xt::noalias(m_a_n) = m_a;
@@ -197,20 +197,20 @@ bool System::timeStep()
     return redraw;
 }
 
-double System::residual() const
+inline double System::residual() const
 {
     double Fext = xt::norm_l2(m_f_frame)();
     double Fnorm = (Fext < std::numeric_limits<double>::epsilon()) ? 1 : Fext;
     return xt::norm_l2(m_f)() / Fnorm;
 }
 
-void System::quench()
+inline void System::quench()
 {
     m_v.fill(0.0);
     m_a.fill(0.0);
 }
 
-size_t System::minimise(double tol, size_t niter_tol, size_t max_iter)
+inline size_t System::minimise(double tol, size_t niter_tol, size_t max_iter)
 {
     GooseFEM::Iterate::StopList stop(niter_tol);
 
@@ -227,7 +227,7 @@ size_t System::minimise(double tol, size_t niter_tol, size_t max_iter)
     throw std::runtime_error("No convergence found");
 }
 
-bool System::advanceRightElastic(double eps)
+inline bool System::advanceRightElastic(double eps)
 {
     double dx = xt::amin(this->yieldDistanceRight())();
     if (dx < eps / 2.0) {
@@ -243,7 +243,7 @@ bool System::advanceRightElastic(double eps)
     return redraw;
 }
 
-bool System::advanceRightKick(double eps)
+inline bool System::advanceRightKick(double eps)
 {
     double deltax = eps;
     m_x += deltax;
@@ -255,72 +255,72 @@ bool System::advanceRightKick(double eps)
     return redraw;
 }
 
-double System::x_frame() const
+inline double System::x_frame() const
 {
     return m_x_frame;
 }
 
-xt::xtensor<double, 1> System::x() const
+inline xt::xtensor<double, 1> System::x() const
 {
     return m_x;
 }
 
-xt::xtensor<double, 1> System::v() const
+inline xt::xtensor<double, 1> System::v() const
 {
     return m_v;
 }
 
-xt::xtensor<double, 1> System::a() const
+inline xt::xtensor<double, 1> System::a() const
 {
     return m_a;
 }
 
-xt::xtensor<double, 1> System::f() const
+inline xt::xtensor<double, 1> System::f() const
 {
     return m_f;
 }
 
-xt::xtensor<double, 1> System::f_potential() const
+inline xt::xtensor<double, 1> System::f_potential() const
 {
     return m_f_potential;
 }
 
-xt::xtensor<double, 1> System::f_frame() const
+inline xt::xtensor<double, 1> System::f_frame() const
 {
     return m_f_frame;
 }
 
-xt::xtensor<double, 1> System::f_neighbours() const
+inline xt::xtensor<double, 1> System::f_neighbours() const
 {
     return m_f_neighbours;
 }
 
-xt::xtensor<double, 1> System::f_damping() const
+inline xt::xtensor<double, 1> System::f_damping() const
 {
     return m_f_damping;
 }
 
-xt::xtensor<double, 1> System::yieldLeft() const
+inline xt::xtensor<double, 1> System::yieldLeft() const
 {
     return m_y.currentYieldLeft();
 }
 
-xt::xtensor<double, 1> System::yieldRight() const
+inline xt::xtensor<double, 1> System::yieldRight() const
 {
     return m_y.currentYieldRight();
 }
 
-xt::xtensor<int, 1> System::yieldIndex() const
+inline xt::xtensor<int, 1> System::yieldIndex() const
 {
     return m_y.currentIndex();
 }
 
-xt::xtensor<double, 1> System::yieldDistanceRight() const
+inline xt::xtensor<double, 1> System::yieldDistanceRight() const
 {
     return m_y.currentYieldRight() - m_x;
 }
 
-xt::xtensor<double, 1> System::yieldDistanceLeft() const
+inline xt::xtensor<double, 1> System::yieldDistanceLeft() const
 {
     return m_x - m_y.currentYieldLeft();
 }
