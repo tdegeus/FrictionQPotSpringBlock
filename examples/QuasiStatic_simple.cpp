c029e6537b24c565b506655ad1ed5a7eff5911c2
Tom de Geus <tom@geus.me>
2022-03-24 17:20:19 +0100

    Simplifying example (historic data `txt` -> `h5`, no change this was checked)

--- examples/QuasiStatic_simple.cpp
+++ examples/QuasiStatic_simple.cpp
@@ -1,8 +1,18 @@
 #include <FrictionQPotSpringBlock/Line1d.h>
+#include <cpppath.h>
 #include <fmt/core.h>
-#include <fstream>
+#include <highfive/H5Easy.hpp>
 #include <prrng.h>
-#include <xtensor/xcsv.hpp>
+
+template <class S, class T>
+void check(const S& source, const T& file, const std::string& path)
+{
+    auto ret = H5Easy::load<S>(file, path);
+
+    if (!xt::allclose(ret, source)) {
+        throw std::runtime_error("Failed to match historic data");
+    }
+}
 
 int main()
 {
@@ -22,19 +32,21 @@ int main()
         1.0, 2.0 * std::sqrt(3.0) / 10.0, 1.0, 1.0, 1.0 / double(N), 0.1, y);
 
     size_t ninc = 1000;
-    xt::xtensor<double, 2> ret = xt::empty<double>({size_t(2), ninc});
+    xt::xtensor<double, 1> ret_x_frame = xt::empty<double>({ninc});
+    xt::xtensor<double, 1> ret_f_frame = xt::empty<double>({ninc});
+    xt::xtensor<long, 1> ret_S = xt::empty<long>({ninc});
 
     for (size_t inc = 0; inc < ninc; ++inc) {
+
+        // Extract output data.
+        auto i_n = sys.i();
 
         // Apply event-driven protocol.
         if (inc == 0) {
             sys.set_x_frame(0.0); // initial quench
         }
-        else if (inc % 2 != 0) {
-            sys.eventDrivenStep(xdelta, false); // elastically advance -> mechanical equilibrium
-        }
         else {
-            sys.eventDrivenStep(xdelta, true); // apply kick
+            sys.eventDrivenStep(xdelta, inc % 2 == 0); // normal event driven step
         }
 
         // Minimise energy.
@@ -44,13 +56,16 @@ int main()
         }
 
         // Extract output data.
-        ret(0, inc) = sys.x_frame();
-        ret(1, inc) = xt::mean(sys.f_frame())();
+        ret_x_frame(inc) = sys.x_frame();
+        ret_f_frame(inc) = xt::mean(sys.f_frame())();
+        ret_S(inc) = xt::sum(sys.i() - i_n)();
     }
 
-    std::ofstream outfile("Load.txt");
-    xt::dump_csv(outfile, ret);
-    outfile.close();
+    auto path = cpppath::join({cpppath::dirname(__FILE__), "QuasiStatic.h5"});
+    H5Easy::File file(path);
+    check(ret_x_frame, file, "x_frame");
+    check(ret_f_frame, file, "f_frame");
+    check(ret_S, file, "S");
 
     return 0;
 }
