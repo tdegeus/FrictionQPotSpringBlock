4e926588694531c34e738eb4891a5e9ecaa33f38
Tom de Geus <tom@geus.me>
2022-11-11 17:53:45 +0100

    Minor style updates `setup.py`

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+from pathlib import Path
+
+from setuptools import find_packages
 from setuptools_scm import get_version
 from skbuild import setup
 
 project_name = "FrictionQPotSpringBlock"
 description = "Spring-block friction model with local disordered potential energy landscape."
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name=project_name,
@@ -13,7 +19,7 @@ setup(
     author="Tom de Geus",
     author_email="tom@geus.me",
     url=f"https://github.com/tdegeus/{project_name}",
-    packages=[f"{project_name}"],
+    packages=find_packages(exclude=["tests"]),
     package_dir={"": "python"},
     cmake_install_dir=f"python/{project_name}",
     cmake_minimum_required_version="3.13...3.21",
