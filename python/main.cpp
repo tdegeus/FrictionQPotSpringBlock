222be53259c080267786f57be0cc24f1864766cd
Tom de Geus <tom@geus.me>
2022-11-16 12:14:53 +0100

    Minor style updates

--- python/main.cpp
+++ python/main.cpp
@@ -1,7 +1,5 @@
 /**
- * Python API
- *
- * @file main.cpp
+ * @file
  * @copyright Copyright 2020. Tom de Geus. All rights reserved.
  * @license This project is released under the GNU Public License (MIT).
  */
